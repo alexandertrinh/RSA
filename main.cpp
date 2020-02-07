920131cfc7f97685850f873580843ae7cec6e1f1
alexandertrinh <alexander9821@gmail.com>
2020-02-06 20:38:15 -0800

    Code update complete, now needs bug fixes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <cmath>
+#include <fstream>
 
 using namespace std;
 
@@ -21,12 +22,15 @@ unsigned int gcd(const unsigned int &e, const unsigned int &phi);
  */
 int main()
 {
-    unsigned int e, n, d, p, q;
+    unsigned int e, n, d, p, q, c, m;
+    std::ifstream inputFile("input.txt");
+    std::ofstream outputFile("output.txt");
     bool isValid_e = false, isValid_n = false, isValid_encrypORdecrypt = false;
     std::string str_e, str_n, str_encrypORdecrypt;
+    char table[50] = {',','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','.','?','!'};
 
 
-    /* Get user public key and make sure it's positive integer */
+/* Get user public key and make sure it's positive integer */
     while (!isValid_e || !isValid_n) {  //if e and n are valid
         promptUser(1);
         std::getline (std::cin, str_e);
@@ -42,7 +46,7 @@ int main()
     }
 
 
-    /* Get user 'e' or 'd' and make sure it's valid */
+/* Get user 'e' or 'd' and make sure it's valid */
     while (!isValid_encrypORdecrypt){   //if 'e' or 'd' given
         promptUser(3);
         std::getline (std::cin, str_encrypORdecrypt);
@@ -54,7 +58,7 @@ int main()
     }
 
 
-    /* Convert string to unsigned int */
+/* Convert string to unsigned int */
     std::cout << "\n";  //newline
     std::stringstream iss_e(str_e);
     iss_e >> e;
@@ -62,7 +66,7 @@ int main()
     iss_n >> n;
 
 
-    /* Decryption 'd' was seleted */
+/* Decryption 'd' was seleted */
     if (str_encrypORdecrypt == "d") {
         p = sqrt(n);            //start p with first odd number thats below the square root of n
         p = findNextOdd(p);     //p is now odd
@@ -71,28 +75,35 @@ int main()
                 q = n/p;
 
                 unsigned int phi = (p-1)*(q-1); //totient of n
-                if ( gcd(e, phi) == 1 ) {
+                if ( gcd(e, phi) == 1 ) {       //make sure e, phi are relatively prime
                     d = find_d(e, phi);         //d = e^-1 mod( (p-1)*(q-1) )
                     break;                      //d found
                 }
             }
             p--;
+        }   //we don't test if n = 2
+
+
+/* d found now convert integer to character*/
+
+        while (inputFile >> c) {
+            m = (unsigned int)pow(c, d) % n;    //decrypt m = c^d mod n
+            outputFile << table[m] << std::endl;
         }
+        outputFile.close();
 
-        //d found now convert text
-        //test 2
-
-    } else
-
-    /* encryption 'e' was selected */
+    } else {
 
 
+/* encryption 'e' was selected */
 
+        while (inputFile >> m) {    //c = m^e mod n
+            c = (unsigned int)pow(m, d) % n;
+            outputFile << to_string(c) << std::endl;
+        }
+        outputFile.close();
 
-
-
-
-
+    }
 
     return 0;
 }
@@ -128,12 +139,15 @@ void promptUser(unsigned int x) {
 }
 
 unsigned int& findNextOdd(unsigned int &x) {
-    x = (x % 2) ? x : x-1;      //if x is even subtract it by 1
+    if ( (x % 2) == 0 )     //x is even
+        x--;
+    else
+        x = x-2;
     return x;
 }
 
 unsigned int find_d(const unsigned int &e, const unsigned int &phi) {
-    //d*e = 1 mod( (p-1)*(q-1) )
+    //d*e = 1 mod(phi)  ;   e^(phi-2)*e = 1 mod(phi)    if e, phi relatively prime
     unsigned int d = (unsigned int)pow(e, phi-2) % phi;
     return d;
 }
