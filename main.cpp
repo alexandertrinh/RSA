1cf691078438da82d21b62a41d452deb89fa8e0c
alexandertrinh <alexander9821@gmail.com>
2020-02-08 22:59:10 -0800

    Working RSA decrypting program

--- main.cpp
+++ main.cpp
@@ -2,14 +2,21 @@
 #include <sstream>
 #include <cmath>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
-bool isValidUnsignedNum(std::string &str);
+bool isValidUnsignedNum(const std::string &str);
+bool isInteger(const double &num);
 void promptUser(unsigned int x);
 unsigned int& findNextOdd(unsigned int &x);
-unsigned int find_d(const unsigned int &e, const unsigned int &phi);
-unsigned int gcd(const unsigned int &e, const unsigned int &phi);
+unsigned int find_d(const unsigned int &phi, const unsigned int &e);
+unsigned int gcd(const unsigned int &phi, const unsigned int &e);
+unsigned int mod(const unsigned int &x, const unsigned int &y, const unsigned int &z);
+std::vector<unsigned int>& getIntsFromLine(std::vector<unsigned int> &vect, std::string line);
+std::vector<unsigned int>& encrypt(vector<unsigned int> &vect, const char table[],
+                                   const std::string &line, const unsigned int &e, const unsigned int &n);
+
 
 /*
  * This program implements RSA. The user will first be prompted for a public key
@@ -20,17 +27,18 @@ unsigned int gcd(const unsigned int &e, const unsigned int &phi);
  * decrypted or encrypted message. The integers corresponding with the alphabetical
  * letter is hard coded in the program.
  */
-int main()
+int main(int argc, char*argv[]) //<fileName> e n 'e'/'d' <outputFileName>
 {
     unsigned int e, n, d, p, q, c, m;
-    std::ifstream inputFile("input.txt");
-    std::ofstream outputFile("output.txt");
+    std::string inputFileName = argv[4];
+    std::ifstream inputFile(inputFileName);
     bool isValid_e = false, isValid_n = false, isValid_encrypORdecrypt = false;
-    std::string str_e, str_n, str_encrypORdecrypt;
-    char table[50] = {',','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','.','?','!'};
+    std::string str_e = argv[1], str_n = argv[2], str_encrypORdecrypt = argv[3], line;
+    char table[50] = {' ', ' ', 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ','.','?','!'};
 
 
 /* Get user public key and make sure it's positive integer */
+/*
     while (!isValid_e || !isValid_n) {  //if e and n are valid
         promptUser(1);
         std::getline (std::cin, str_e);
@@ -44,10 +52,10 @@ int main()
         if (!isValid_e || !isValid_n)
             promptUser(4);
     }
-
+*/
 
 /* Get user 'e' or 'd' and make sure it's valid */
-    while (!isValid_encrypORdecrypt){   //if 'e' or 'd' given
+/*    while (!isValid_encrypORdecrypt){   //if 'e' or 'd' given
         promptUser(3);
         std::getline (std::cin, str_encrypORdecrypt);
 
@@ -56,10 +64,10 @@ int main()
         else
             break;
     }
+*/
 
 
 /* Convert string to unsigned int */
-    std::cout << "\n";  //newline
     std::stringstream iss_e(str_e);
     iss_e >> e;
     std::stringstream iss_n(str_n);
@@ -75,8 +83,8 @@ int main()
                 q = n/p;
 
                 unsigned int phi = (p-1)*(q-1); //totient of n
-                if ( gcd(e, phi) == 1 ) {       //make sure e, phi are relatively prime
-                    d = find_d(e, phi);         //d = e^-1 mod( (p-1)*(q-1) )
+                if ( gcd(phi, e) == 1 ) {       //make sure e, phi are relatively prime
+                    d = find_d(phi, e);         //d = e^-1 mod( (p-1)*(q-1) )
                     break;                      //d found
                 }
             }
@@ -85,11 +93,18 @@ int main()
 
 
 /* d found now convert integer to character*/
+        std::ofstream outputFile("decrypted.txt");
+        std::vector<unsigned int> intVector;
 
-        while (inputFile >> c) {
-            m = (unsigned int)pow(c, d) % n;    //decrypt m = c^d mod n
-            outputFile << table[m] << std::endl;
+        while (std::getline(inputFile, line)) {
+            intVector = getIntsFromLine(intVector, line);
+            for (unsigned int i = 0; i < intVector.size(); i++) {   //for each integer in file
+                m = mod(intVector.at(i), d, n);   //decrypt m = c^d mod n
+                outputFile << table[m];
+            }
+            intVector.clear();
         }
+        inputFile.close();
         outputFile.close();
 
     } else {
@@ -97,10 +112,21 @@ int main()
 
 /* encryption 'e' was selected */
 
-        while (inputFile >> m) {    //c = m^e mod n
-            c = (unsigned int)pow(m, d) % n;
-            outputFile << to_string(c) << std::endl;
+        //read in line and get each character
+        std::ofstream outputFile("incrypted.txt");
+        std::vector<unsigned int> encryptedLineVector;
+
+        while (std::getline(inputFile, line)) {
+            encryptedLineVector = encrypt(encryptedLineVector, table, line, e, n);  //c = m^e mod n
+
+            for (unsigned int i = 0; i < encryptedLineVector.size(); i++) {
+                outputFile << to_string( encryptedLineVector.at(i) );   //append unsigned int to string to file
+                if (i != encryptedLineVector.size()-1)  //if last letter no space
+                    outputFile << " ";
+            }
+            encryptedLineVector.clear();
         }
+        inputFile.close();
         outputFile.close();
 
     }
@@ -108,7 +134,7 @@ int main()
     return 0;
 }
 
-bool isValidUnsignedNum(std::string &str) {
+bool isValidUnsignedNum(const std::string &str) {
     std::string::const_iterator iterator;
     iterator = str.begin();
 
@@ -146,16 +172,83 @@ unsigned int& findNextOdd(unsigned int &x) {
     return x;
 }
 
-unsigned int find_d(const unsigned int &e, const unsigned int &phi) {
-    //d*e = 1 mod(phi)  ;   e^(phi-2)*e = 1 mod(phi)    if e, phi relatively prime
-    unsigned int d = (unsigned int)pow(e, phi-2) % phi;
-    return d;
+unsigned int find_d(const unsigned int &phi, const unsigned int &e) {
+    //d*e = k*phi + 1  ->   d = (k*phi + 1) / e;
+    unsigned int k = 1;
+    double num;
+
+    while (true) {
+        num = k*phi + 1;
+        num = num / e;
+        if (isInteger(num))
+            break;
+        k++;
+    }
+
+    return (unsigned int)num;
 }
 
-unsigned int gcd(const unsigned int &e, const unsigned int &phi) {
-    if (e == phi)   //recursive algorithm to find gcd
-        return e;
-    if (e < phi)
-        return gcd(phi-e, e);
-    return gcd(e-phi, phi);
+bool isInteger(const double &num) {
+    unsigned int intNum = (unsigned int)num;
+    double doubleNum = (double)intNum;
+    if (doubleNum == num)
+        return true;
+    else
+        return false;
+}
+
+unsigned int gcd(const unsigned int &phi, const unsigned int &e) {
+    if (e == 0)   //recursive algorithm to find gcd
+        return phi;
+    return gcd(e, phi%e);
+}
+
+unsigned int mod(const unsigned int &x, const unsigned int &y, const unsigned int &z) {
+    //x^y mod z
+    unsigned int i = x, j = y;
+    while (j > 1) {
+        i = (i*x) % z;
+        j--;
+    }
+    return i;
+}
+
+std::vector<unsigned int>& getIntsFromLine(std::vector<unsigned int> &vect,
+                                           std::string line) {
+    size_t pos;
+    std::string intStr;
+    unsigned int num;
+
+    while ( (pos = line.find(" ")) != std::string::npos ) {
+        intStr = line.substr(0, pos);
+        std::stringstream iss(intStr);
+        iss >> num;
+        vect.push_back(num);
+        line.erase(0, intStr.length() + 1);
+    }
+    std::stringstream iss(line);
+    iss >> num;
+    vect.push_back(num);
+
+    return vect;
+}
+
+std::vector<unsigned int>& encrypt(vector<unsigned int> &vect, const char table[],
+                                   const std::string &line, const unsigned int &e,
+                                   const unsigned int &n) {
+
+    unsigned int c;
+    char letter;
+
+    for (unsigned int i = 0; i < line.length(); i++) {  //for every character in line
+        for (unsigned int j = 2; j < 35; j++) { //check for matching letter in table array
+            letter = line.at(i);
+            if (letter == table[j]) {
+                c = mod(j, e, n); //c = m^e mod n
+                vect.push_back(c);
+            }
+        }
+    }
+
+    return vect;
 }
